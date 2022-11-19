c6da55bd535dedf04f5892e5d91bf0a4b730e94c
simon stoneman <simon.stoneman@yahoo.co.uk>
2022-11-19 16:24:24 -0500

    On checking prompt input, i changed to using is NaN func instead of null. Added counter for user selections, so that if none is selected, prompt to select something and restart genPass func. Tidied up var decl and added more comms

--- challenge/assets/js/script.js
+++ challenge/assets/js/script.js
@@ -1,3 +1,4 @@
+// Start of Global Data
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
@@ -87,12 +88,15 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
+// End of Global Data
 
-var arrayArrays = [lowerCasedCharacters,upperCasedCharacters,numericCharacters,specialCharacters];
+// Start of Global Vars
+var arrayOfAllArrays = [lowerCasedCharacters,upperCasedCharacters,numericCharacters,specialCharacters];
 var lowerCharsIndx = 0;
 var upperCharsIndx = 1;
 var numCharsIndx = 2;
 var specialCharsIndx = 3;
+// End of Global Vars
 
 // Function to prompt user for password options
 function getPasswordOptions() {
@@ -103,20 +107,26 @@ function getPasswordOptions() {
   let specialChars = false;
   let pwdArray = [];
   let pwdLength = 0;
+  let selectionCnt = 0;
 
   //prompt for password length (between 10 - 64 chars)
   pwdLength = prompt("Enter the required password length between between 10 and 64:");
 
   //check length of user password length requirement is within boundaries
-  if (pwdLength == null){
-    
+  if (isNaN(pwdLength)){
+
+    // if value entered is not a number, then alert and re-run generatePassword() func
+
       alert('Please enter a valid value');
-      
+
       generatePassword();
 
   } else if (pwdLength < 10 || pwdLength > 64) {
 
+      // if number entered outside of range, then alert and re-run generatePassword() func
+
       alert('Please enter a number between 10 and 64');
+
       generatePassword();
 
   } else {
@@ -124,34 +134,51 @@ function getPasswordOptions() {
       lowerChars = confirm('Do you want the password to contain lowercase characters?');
       
       if(lowerChars){
-        pwdArray.push(arrayArrays[lowerCharsIndx]);
+        //Select the nested lowerChars array data and add to custom pwdArray
+        pwdArray.push(arrayOfAllArrays[lowerCharsIndx]);
+        selectionCnt ++;
       }
 
       // confirm for wanting uppercase
       upperChars = confirm('Do you want the password to contain uppercase characters?');
       
       if(upperChars){
-        pwdArray.push(arrayArrays[upperCharsIndx]);
+        //Select the nested upperChars array data and add to custom pwdArray
+        pwdArray.push(arrayOfAllArrays[upperCharsIndx]);
+        selectionCnt ++;
       }
 
       // confirm for wanting numeric
       numChars = confirm('Do you want the password to contain numeric characters?');
       
       if(numChars){
-        pwdArray.push(arrayArrays[numCharsIndx]);
+        //Select the nested numChars array data and add to custom pwdArray
+        pwdArray.push(arrayOfAllArrays[numCharsIndx]);
+        selectionCnt ++;
       }
 
       // confirm for wanting special chars
       specialChars = confirm('Do you want the password to contain special characters?');
       
       if(specialChars){
-        pwdArray.push(arrayArrays[specialCharsIndx]);
+        //Select the nested specialChars array data and add to custom pwdArray
+        pwdArray.push(arrayOfAllArrays[specialCharsIndx]);
+        selectionCnt ++;
       }
+ 
+      //One last selection if the selection count equals zero, then re-run generatePassword() func.
+      if (selectionCnt < 1){
+        alert('Please selected an option');
+
+        generatePassword();
+      }
+      
   }
 
+  // return the custom concat array based on the users selections and the required password length as an object
   return {
     selArray: pwdArray, 
-    passwlength: pwdLength
+    selPasswLength: pwdLength
   };
 
 }
@@ -159,13 +186,15 @@ function getPasswordOptions() {
 // Function for getting a random element from an array
 function getRandom(arr) {
 
-  var randomArrayIndex = 0;
-  var randomArrayElement = '';
+  let randomArrayIndex = 0;
+  let randomArrayElement = '';
+
+  //console.log("Array being processed is :" + arr);
 
   randomArrayIndex = Math.floor(Math.random() * arr.length);
-  console.log('Random Array Index is: ' + randomArrayIndex);
+  //console.log('Random Array Index is: ' + randomArrayIndex);
   randomArrayElement = arr [randomArrayIndex];
-  console.log('Random Array Element is: ' + randomArrayElement);
+  //console.log('Random Array Element is: ' + randomArrayElement);
 
   return randomArrayElement;
 
@@ -174,28 +203,40 @@ function getRandom(arr) {
 // Function to generate password with user input
 function generatePassword() {
 
-let genPassword = '';
-//Retrieve getPasswordOptions function outputs
-let gPOOutput = getPasswordOptions();
+  let genPassword = '';
+  let gpoFuncOutput = {};
+  let gpoArray = '';
+  let gpoPwdLength = '';
 
-let gpoArray = gPOOutput.selArray;
-let gpoPwdLength = gPOOutput.passwlength;
 
-//console.log('User Selected Array Content is: ' + gpoArray);
+  //Retrieve getPasswordOptsions function outputs (an object)
+  gpoFuncOutput = getPasswordOptions();
 
-// console.log('User Selected Password Length is: ' + gpoPwdLength);
+  //Set gpoArray to value of gpoFuncOutput (object) selArray property
+  gpoArray = gpoFuncOutput.selArray;
 
-for (let i = 0; i <= gpoPwdLength; ++i){
-  let gpoArrayRandomArrayIndex = Math.floor(Math.random() * gpoArray.length);
-  genPassword += getRandom(gpoArray[gpoArrayRandomArrayIndex]);
-};
+  //Set gpoArray to value of gpoFuncOutput (object) passwLength property
+  gpoPwdLength = gpoFuncOutput.selPasswLength;
 
-//combine user selected array types in to one array
-//iterate through combined array for length of pwd 
+  //console.log('User Selected Array Content is: ' + gpoArray);
 
-//console.log('Generated Password is: ' + genPassword);
+  //console.log('User Selected Password Length is: ' + gpoPwdLength);
 
-return genPassword;
+  for (let i = 0; i <= gpoPwdLength; ++i){
+
+    //console.log('User Gen Array It: ' + i);
+
+    // Get a random index number based on the four combined primary arrays e.g. lowercase, uppercase.... total of 4 - 1 (0 index)
+    let gpoArrayRandomArrayIndex = Math.floor(Math.random() * gpoArray.length);
+
+    // Concate each random password char from the randomly selected array type (of 4) and its vals
+    genPassword += getRandom(gpoArray[gpoArrayRandomArrayIndex]);
+  };
+
+  //console.log('Generated Password is: ' + genPassword);
+
+  // Return the concate password string for presentation
+  return genPassword;
 }
 
 // Get references to the #generate element
@@ -203,6 +244,8 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
+
+  // Create variable 'password' and set value to what is returned from executing generatePassword() func 
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
