2d91e340d665806bafbfc749a7d63d6d55a5be05
simon stoneman <simon.stoneman@yahoo.co.uk>
2022-11-19 12:40:56 -0500

    Fixed script execution location path. Now have working generator showing correct size and defined included char/sym/num's

--- challenge/assets/js/script.js
+++ challenge/assets/js/script.js
@@ -88,41 +88,111 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var arrayArrays = [lowerCasedCharacters,upperCasedCharacters,numericCharacters,specialCharacters];
+var lowerCharsIndx = 0;
+var upperCharsIndx = 1;
+var numCharsIndx = 2;
+var specialCharsIndx = 3;
+
 // Function to prompt user for password options
 function getPasswordOptions() {
 
-  var lowerChars = '';
-  var upperChars = '';
-  var numChars = '';
-  var specialChars = '';
-  var pwdLength = 0;
+  let lowerChars = false;
+  let upperChars = false;
+  let numChars = false;
+  let specialChars = false;
+  let pwdArray = [];
+  let pwdLength = 0;
 
   //prompt for password length (between 10 - 64 chars)
   pwdLength = prompt("Enter the required password length between between 10 - 64:");
-  // confirm for wanting lowercase
-  lowerChars = confirm('Do you want the password to contain lowercase characters?');
-  // confirm for wanting uppercase
-  upperChars = confirm('Do you want the password to contain uppercase characters?');
-  // confirm for wanting numeric
-  numChars = confirm('Do you want the password to contain numeric characters?');
-  // confirm for wanting special chars
-  specialChars = confirm('Do you want the password to contain special characters?');
+
+  //check length of user password length requirement is within boundaries
+  if (pwdLength == null){
+    
+      prompt('Please enter a value');
+
+  } else if (pwdLength < 10 || pwdLength > 64) {
+
+      prompt('Please enter a number between 10 and 64');
+
+  } else {
+      // confirm for wanting lowercase
+      lowerChars = confirm('Do you want the password to contain lowercase characters?');
+      
+      if(lowerChars){
+        pwdArray.push(arrayArrays[lowerCharsIndx]);
+      }
+
+      // confirm for wanting uppercase
+      upperChars = confirm('Do you want the password to contain uppercase characters?');
+      
+      if(upperChars){
+        pwdArray.push(arrayArrays[upperCharsIndx]);
+      }
+
+      // confirm for wanting numeric
+      numChars = confirm('Do you want the password to contain numeric characters?');
+      
+      if(numChars){
+        pwdArray.push(arrayArrays[numCharsIndx]);
+      }
+
+      // confirm for wanting special chars
+      specialChars = confirm('Do you want the password to contain special characters?');
+      
+      if(specialChars){
+        pwdArray.push(arrayArrays[specialCharsIndx]);
+      }
+  }
+
+  return {
+    selArray: pwdArray, 
+    passwlength: pwdLength
+  };
+
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  var randomArrayIndex = '';
+
+  var randomArrayIndex = 0;
   var randomArrayElement = '';
 
-  randomArrayIndex = math.floor(math.random() * arr.length);
+  randomArrayIndex = Math.floor(Math.random() * arr.length);
+  console.log('Random Array Index is: ' + randomArrayIndex);
   randomArrayElement = arr [randomArrayIndex];
+  console.log('Random Array Element is: ' + randomArrayElement);
 
   return randomArrayElement;
+
 }
 
 // Function to generate password with user input
 function generatePassword() {
 
+let genPassword = '';
+//Retrieve getPasswordOptions function outputs
+let gPOOutput = getPasswordOptions();
+
+let gpoArray = gPOOutput.selArray;
+let gpoPwdLength = gPOOutput.passwlength;
+
+//console.log('User Selected Array Content is: ' + gpoArray);
+
+// console.log('User Selected Password Length is: ' + gpoPwdLength);
+
+for (let i = 0; i <= gpoPwdLength; ++i){
+  let gpoArrayRandomArrayIndex = Math.floor(Math.random() * gpoArray.length);
+  genPassword += getRandom(gpoArray[gpoArrayRandomArrayIndex]);
+};
+
+//combine user selected array types in to one array
+//iterate through combined array for length of pwd 
+
+//console.log('Generated Password is: ' + genPassword);
+
+return genPassword;
 }
 
 // Get references to the #generate element
@@ -133,6 +203,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
+ // console.log('Password value from generatePassword() is: ' + password);
   passwordText.value = password;
 }
 
